433c914c0f43cb40de992faa94223a0fe264e566
gouravkotnala <gouravkotnala777@gmail.com>
2024-10-21 11:14:01 +0530

    style : add messages ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -139,47 +139,59 @@ const Home = () => {
                             <div className="middle_part">
                                 <div className="messages_cont">
                                     
-                                    <div className="message_cont">
+                                    <div className="incomming_message_cont">
+                                        <div className="content">H</div>
+                                        <div className="date">8:05 am</div>
+                                    </div>
+                                    <div className="incomming_message_cont">
+                                        <div className="content">He</div>
+                                        <div className="date">8:05 am</div>
+                                    </div>
+                                    <div className="incomming_message_cont">
+                                        <div className="content">Hello</div>
+                                        <div className="date">8:05 am</div>
+                                    </div>
+                                    <div className="default_message_cont">
+                                        <div className="content">Hello</div>
+                                        <div className="date">8:05 am</div>
+                                    </div>
+                                    <div className="outgoing_message_cont">
+                                        <div className="content">Hello</div>
+                                        <div className="date">8:05 am</div>
+                                    </div>
+                                    <div className="incomming_message_cont">
+                                        <div className="content">Hello my</div>
+                                        <div className="date">8:05 am</div>
+                                    </div>
+                                    <div className="incomming_message_cont">
+                                        <div className="content">Hello my name</div>
+                                        <div className="date">8:05 am</div>
+                                    </div>
+                                    <div className="outgoing_message_cont">
+                                        <div className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae dolore accusantium obcaecati distinctio odit facilis fugiat, omnis quia voluptas. Deleniti modi unde aspernatur similique excepturi dolorum commodi adipisci voluptas quasi.</div>
+                                        <div className="date">8:05 am</div>
+                                    </div>
+                                    <div className="incomming_message_cont">
                                         <div className="content">Hello my name is gourav</div>
                                         <div className="date">8:05 am</div>
                                     </div>
-                                    <div className="message_cont">
+                                    <div className="incomming_message_cont">
+                                        <div className="content">Hello my name is gourav skdjsdaj sdkoajsd sakdjkajssn dsdjkasd ksjdkas ksjdkl lkjsdkaj asfhuhdc w wcjwenkmdniois dashdnsma</div>
+                                        <div className="date">8:05 am</div>
+                                    </div>
+                                    <div className="incomming_message_cont">
                                         <div className="content">Hello my name is gourav</div>
                                         <div className="date">8:05 am</div>
                                     </div>
-                                    <div className="message_cont">
+                                    <div className="incomming_message_cont">
                                         <div className="content">Hello my name is gourav</div>
                                         <div className="date">8:05 am</div>
                                     </div>
-                                    <div className="message_cont">
+                                    <div className="incomming_message_cont">
                                         <div className="content">Hello my name is gourav</div>
                                         <div className="date">8:05 am</div>
                                     </div>
-                                    <div className="message_cont">
-                                        <div className="content">Hello my name is gourav</div>
-                                        <div className="date">8:05 am</div>
-                                    </div>
-                                    <div className="message_cont">
-                                        <div className="content">Hello my name is gourav</div>
-                                        <div className="date">8:05 am</div>
-                                    </div>
-                                    <div className="message_cont">
-                                        <div className="content">Hello my name is gourav</div>
-                                        <div className="date">8:05 am</div>
-                                    </div>
-                                    <div className="message_cont">
-                                        <div className="content">Hello my name is gourav</div>
-                                        <div className="date">8:05 am</div>
-                                    </div>
-                                    <div className="message_cont">
-                                        <div className="content">Hello my name is gourav</div>
-                                        <div className="date">8:05 am</div>
-                                    </div>
-                                    <div className="message_cont">
-                                        <div className="content">Hello my name is gourav</div>
-                                        <div className="date">8:05 am</div>
-                                    </div>
-                                    <div className="message_cont">
+                                    <div className="incomming_message_cont">
                                         <div className="content">Hello my name is gourav</div>
                                         <div className="date">8:05 am</div>
                                     </div>
